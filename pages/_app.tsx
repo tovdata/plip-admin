3767c60c903fa6abb0355d0cff363fa81525f56f
hmin01 <goals1003@gmail.com>
2023-04-06 16:18:03 +0900

    2023.04.06 - Dynamic import를 이용한 최적화 적용

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,17 @@
+import dynamic from "next/dynamic";
 import localFont from "next/font/local";
 // Component
-import { StyleProvider } from "@ant-design/cssinjs"
-import Authorization from '@/components/templates/Auth';
-import GlobalQueryProvider from '@/components/templates/Provider';
+import { ConfigProvider } from 'antd';
+import { StyleProvider } from '@ant-design/cssinjs';
+// Component (dynamic)
+const Authorization: ComponentType<any> = dynamic(() => import('@/components/templates/Auth'));
+const GlobalQueryProvider: ComponentType<any> = dynamic(() => import('@/components/templates/Provider'));
 // Data type
-import type { ThemeConfig } from "antd";
-import type { AppProps } from "next/app";
+import type { ThemeConfig } from 'antd';
+import type { AppProps } from 'next/app';
+import type { ComponentType } from 'react';
 // Style
 import '@/styles/globals.css';
-import { ConfigProvider } from "antd";
 
 // Local font
 const font = localFont({
@@ -51,15 +54,15 @@ const theme: ThemeConfig = {
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ConfigProvider theme={theme}>
-        <GlobalQueryProvider>
-          <StyleProvider hashPriority="high">
-            <main className={`bg-inherit ${font.className}`}>
-              <Authorization>
-                <Component {...pageProps} />
-              </Authorization>
-            </main>
-          </StyleProvider>
-        </GlobalQueryProvider>
+      <GlobalQueryProvider>
+        <StyleProvider hashPriority="high">
+          <main className={`bg-inherit ${font.className}`}>
+            <Authorization>
+              <Component {...pageProps} />
+            </Authorization>
+          </main>
+        </StyleProvider>
+      </GlobalQueryProvider>
     </ConfigProvider>
   );
 }

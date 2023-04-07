11e472983f31fda8f997e7cf085ada5d6c664c8e
hmin01 <goals1003@gmail.com>
2023-04-07 10:55:55 +0900

    2023.04.07 - jotai를 이용한 권한 팝업 상태 관리 로직 수정

--- components/templates/Auth.tsx
+++ components/templates/Auth.tsx
@@ -3,7 +3,7 @@ import React, { useEffect } from 'react';
 // Component
 import { Modal } from 'antd';
 // Status
-import { useAuthPopupValue } from '@/models/jotai/atoms/authPopup';
+import { useAuthPopupReset, useAuthPopupValue } from '@/models/jotai/atoms/authPopup';
 
 /** [Component] 인증 */
 export const Authorization: React.FC<any> = ({ children }: { children: React.ReactNode }) => {
@@ -11,13 +11,20 @@ export const Authorization: React.FC<any> = ({ children }: { children: React.Rea
   const router = useRouter();
   // 팝업 표시 상태
   const { open } = useAuthPopupValue();
+  // 상태 초기화 함수
+  const reset = useAuthPopupReset();
 
   /** [Event hook] 권한 없음에 대한 팝업 열기 */
   useEffect((): void => {
     if (open) {
       Modal.warn({
         centered: true,
-        onOk: (): Promise<boolean> => router.push('/login'),
+        onOk: (): void => {
+          // 팝업 표시 상태 초기화
+          reset();
+          // 페이지 이동
+          router.push('/login');
+        },
         title: "로그인이 만료되었습니다. 다시 로그인 해주세요."
       });
     }
--- models/jotai/atoms/authPopup.ts
+++ models/jotai/atoms/authPopup.ts
@@ -1,4 +1,5 @@
-import { atom, useAtomValue, useSetAtom } from 'jotai';
+import { useAtomValue, useSetAtom } from 'jotai';
+import { atomWithReset, useResetAtom } from 'jotai/utils';
 
 // 초기 상태 타입
 interface InitialStateType {
@@ -11,7 +12,8 @@ const initialState: InitialStateType = {
 }
 
 // 로그인 권한에 따른 팝업 상태 atom 생성
-const authPopupAtom = atom<InitialStateType>(initialState);
+const authPopupAtom = atomWithReset<InitialStateType>(initialState);
 
 export const useAuthPopupValue = () => useAtomValue(authPopupAtom);
 export const useAuthPopupSetter = () => useSetAtom(authPopupAtom);
+export const useAuthPopupReset = () => useResetAtom(authPopupAtom);
